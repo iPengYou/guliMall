b4d0e1e4c6c090dedb2b98055858b6b2f6e83471
bigShuai <2137692702@qq.com>
2022-05-30 16:46:58 +0800

    项目初始结构创建

---
+++ guliMall-order/src/test/java/com/yjf/gulimall/gulimall/order/GuliMallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjf.gulimall.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
